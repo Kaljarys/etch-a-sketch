9f5bd95a8b1a71bb781f1e93b5ed6f0c6b33aef8
Kaljarys <kbrantone@gmail.com>
2019-11-28 23:42:32 +0100

    grid OK

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+<<<<<<< HEAD
 
+=======
+gridGeneration(16);
+
+
+
+function gridGeneration(row){
+    var container = document.getElementById("container");
+    container.style.gridTemplateColumns = " 1fr".repeat(row);
+    container.style.gridTemplateRows = " 1fr".repeat(row); 
+    for(var i = 0; i < row*row; i++){
+            const grid = document.createElement("div");
+            grid.className = "grid";
+            container.appendChild(grid); 
+    }
+}
+
+document.getElementByClass("grid").addEventListener("mouseover", function(hover{
+    
+    hover.grid.className = "hover";
+});
+
+
+    
+
+>>>>>>> grid OK
