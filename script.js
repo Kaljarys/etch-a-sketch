cf3b29d7c04ecd21d1f164cc5e84c7717bd7fffb
Kaljarys <31316842+Kaljarys@users.noreply.github.com>
2019-11-29 19:53:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-var container = document.getElementById("container");
-var grid = document.createElement("div"); 
-grid.setAttribute("class", "grid");
-var row = 16; 
-var column = row; 
-var gridSize = 700 / row;
-grid.style.width = gridSize + "px";
-grid.style.height = gridSize + "px"; 
 
-function gridGeneration(row, column){
-    for(var i = 0; i < column; i++){
-        for(var e = 0; e < row; e++){
-            container.appendChild(grid);
-            console.log("grid created" + e + i);  
-        }
-    }
-}
-gridGeneration(row, column);
