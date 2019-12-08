b8634e4b9aeec020b1f4a5a3abf2282f5c07d832
ThomasGodard <thomasgodard2@gmail.com>
2019-12-08 13:29:14 +0100

    Refactoring

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 gridGeneration(16);
 
+// Size Input Event Action
+// J'attache une seule et unique fois l'event 'click' sur le bouton (en règle générale: un seul event 'clic' par bouton).
+// A chaque clic il executera la fonction (() => {...}) passée en parametre
+document
+    .querySelector("#selectSize")
+    .addEventListener("click", () => {
+        var number = window.prompt("How many rows?");
+        var emptyGrid = document.createElement('div')
+        document.getElementById("house").replaceChild(emptyGrid, document.getElementById('container'))
+        emptyGrid.id = 'container'
+        gridGeneration(number);
+    })
+
+// Clear Grid Event Action
+// idem
+document
+    .querySelector("#clearGrid") 
+    .addEventListener("click", () => {
+        var grids = getGrids()
+        grids.forEach((grid) => {
+            grid.style.background = "white";
+        })
+    })
 
 
-
-function gridGeneration(row){
+function gridGeneration(row) {
     var container = document.getElementById("container");
     container.style.gridTemplateColumns = " 1fr".repeat(row);
-    container.style.gridTemplateRows = " 1fr".repeat(row); 
-    for(var i = 0; i < row*row; i++){
-            const grid = document.createElement("div");
-            grid.className = "grid";
-            container.appendChild(grid); 
+    container.style.gridTemplateRows = " 1fr".repeat(row);
+    for (var i = 0; i < row * row; i++) {
+        const grid = document.createElement("div");
+        grid.className = "grid";
+        container.appendChild(grid);
     }
-    
-    var grids = document.querySelectorAll(".grid");
-        grids.forEach((grid) => {
-            grid.addEventListener("mouseover", (e) => {
+    var grids = getGrids()
+    // ici on laisse le addEventListener dans la fonction car on detruit les cases pour recréer notre nouvelle grille
+    // il faudra donc re-attacher le mouseover pour chaque case 
+    grids.forEach((grid) => {
+        grid.addEventListener("mouseover", (e) => {
             e.target.style.background = 'black';
+        })
     })
-})
-    var clearGrid = document.querySelector("#clearGrid");
-        grids.forEach((grid) => {
-            clearGrid.addEventListener("click", (e) => {
-            grid.style.background = "white";
-    })
-})
-
-    let selectSize =  document.querySelector("#selectSize");
-
-    selectSize.addEventListener("click", () => {
-         var number = window.prompt("How many rows?");  
-             grids.forEach((grid) => {
-            document.getElementById("container").removeChild(grid);
-     })
-    gridGeneration(number);
-})
 }
 
-
-
-
-
-
-
-
- 
-
+function getGrids() {
+    return document.querySelectorAll(".grid");
+}
